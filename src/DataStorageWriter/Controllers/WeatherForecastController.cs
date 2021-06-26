f73a295eff5565c0efdbadbb585a489ac808e6c3
Pavel <pavel.saniuk@gmail.com>
2021-06-26 17:09:26 +0200

    Solution, DataStorageReader and DataStorageWriter projects, docker file, docker compose with localstack

---
+++ src/DataStorageWriter/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DataStorageWriter.Controllers
+{
+    [ApiController]
+    [Route("writer")]
+    public class WriterController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WriterController> _logger;
+
+        public WriterController(ILogger<WriterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Put()
+        {
+            
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
